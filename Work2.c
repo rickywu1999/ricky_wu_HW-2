03012e7e57ba2b97fa86467c69343cf222553fd7
Cynthia Wu <ricky1999@Cynthias-Air.nyc.rr.com>
2016-10-10 20:19:17 -0400

    Note to self: cannot print a freed list

--- Work2.c
+++ Work2.c
@@ -4,61 +4,56 @@
 
 struct node { int i; struct node *next ;};
 
-int print_list(struct node *a){
-    struct node *pointer = a;
+void print_list(struct node *a){
     printf("Printing list...\n");
-    while(pointer->next){
-        printf("%d\n",pointer->i);
-        pointer = pointer->next;
+    while(a){
+        printf("%d\n",a->i);
+        a = a->next;
     }
-    printf("%d\n",pointer->i);
-    return 0;
 }
 
 struct node * insert_front(struct node *a, int b){
-  struct node *newnode;
-  newnode = (struct node *) malloc(sizeof(struct node));
-  newnode->i=b;
-  newnode->next=a;
-  return newnode;
+    struct node *newnode;
+    newnode = (struct node *) malloc(sizeof(struct node));
+    newnode->i=b;
+    newnode->next=a;
+    return newnode;
 }
 
 struct node * free_list(struct node *a){
-  struct node *copy = a;
-  struct node *original = a;
-  while (copy->next){
-    copy= copy->next;
-    free(a);
-    a = copy;
-  }
-  free(a);
-  return original;
+    struct node *copy,*original = a;
+    while(a){
+        copy = a->next;
+        free(a);
+        a = copy;
+    }
+    return original;
 }
     
     
 int main(){
-  struct node *node1;
-  struct node *node2;
-  struct node *node3;
-  node1 = (struct node *) malloc(sizeof(struct node));
-  node2 = (struct node *) malloc(sizeof(struct node));
-  node3 = (struct node *) malloc(sizeof(struct node));
-  node1->i = 2;
-  node2->i = 3;
-  node3->i = 4;
-  node1->next = node2;
-  node2->next = node3;
-  node3->next = 0;
-  struct node *head = node1;
+    struct node *node1;
+    struct node *node2;
+    struct node *node3;
+    node1 = (struct node *) malloc(sizeof(struct node));
+    node2 = (struct node *) malloc(sizeof(struct node));
+    node3 = (struct node *) malloc(sizeof(struct node));
+    node1->i = 2;
+    node2->i = 3;
+    node3->i = 4;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = 0;
+    struct node *head = node1;
   
-  print_list(head);
+    print_list(head);
 
-  printf("Adding 1 to the front...\n");
-  head = insert_front(head, 1);
-  print_list(head);
+    printf("Adding 1 to the front...\n");
+    head = insert_front(head, 1);
+    print_list(head);
 
-  printf("Freeing...\n");
-  print_list(free_list(head));
-
-  return 0;
+    printf("Freeing...\n");
+    free_list(head);
+    printf("Completed!\n");
+    return 0;
 }
